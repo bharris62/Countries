42c11e4c7f065c4de821fd65cb4028ef9a45fe74
Andrew Harris <blakebharris@gmail.com>
2017-02-09 15:58:15 -0600

    bug in not finishing the loop

--- src/Main.java
+++ src/Main.java
@@ -6,44 +6,62 @@ import java.io.IOException;
 import java.util.*;
 
 public class Main {
+    static Map<String, List<Country>> worldMap = new HashMap<>();
 
     public static void main(String[] args) throws FileNotFoundException {
-        Map<String, List<Country>> worldMap = new HashMap<>();
-        String letters = "abcdefghijklmnopqrstuvwxyz";
 
-        //read in file.
         File f = new File("countries.txt");
         Scanner scanner = new Scanner(f);
+        addCurrentLetter(scanner);
+        printMap();
+
+
+    }
+
+    public static void addCurrentLetter(Scanner scanner){
+        String letters = "abcdefghijklmnopqrstuvwxyz";
+
+        for(int i=0; i < letters.length(); i++) {
+            addToArray(letters.substring(i,i+1), scanner);
+        }
+    }
+
+    public static void addToArray(String letter, Scanner scanner){
         List<Country> countries = new ArrayList<>();
         int i = 0;
         while (scanner.hasNext()) {
 
             String line = scanner.nextLine();
             String[] columns = line.split("\\|");
-            Country country = new Country(columns[0], columns[1]);
-
-            if(columns[1].charAt(0) == (letters.charAt(i))){
+            if(i > 0){
+                worldMap.put(letter, countries);
+                break;
+            }
+            if(columns[1].substring(i,i+1).equals(letter)){
+                Country country = new Country(columns[0], columns[1]);
                 countries.add(country);
             }else{
-                addtoHash(letters.substring(i, i+1), worldMap, countries);
                 i++;
-
             }
         }
 
+
+
+    }
+
+
+    public static void printMap(){
         for(String name : worldMap.keySet()) {
             String key = name.toString();
             System.out.printf("%-8s", key + ": ");
             for (Country letter : worldMap.get(name)) {
-                System.out.print(letter.name + " ");
+                System.out.print(letter.name + " |");
 
             }
             System.out.println();
         }
 
     }
-
-
 
     public static void addtoHash(String letter, Map<String, List<Country>> worldMap, List<Country> countries){
         worldMap.put(letter, countries);
